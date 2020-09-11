4b56e46a7269f1bdf794297397ec67f1beaaf003
nelsonlejandrosaz <nelsonalejandrosaz@gmail.com>
2020-09-11 00:19:48 -0600

    Agregando modelos
    Se agregaron los modelos Pais, Departamento, Municipios.

---
+++ HCProjectAdmin/Models/Departamento.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HCProjectAdmin.Models
+{
+    public class Departamento
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Nombre { get; set; }
+
+        public Pais Pais { get; set; }
+        [Required]
+        public int PaisId { get; set; }
+    }
+}
---
+++ HCProjectAdmin/Models/Pais.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HCProjectAdmin.Models
+{
+    public class Pais
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(50)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [MaxLength(2)]
+        public string CodigoIso { get; set; }
+
+        public string CodigoTelefono { get; set; }
+    }
+}
